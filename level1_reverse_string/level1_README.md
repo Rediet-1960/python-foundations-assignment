e11f62bcb0ff9be8dba03a78c1ce384d2521cc07
Rediet-1960 <redietwondimu42@gmail.com>
2026-04-06 00:51:29 +0300

    Add files via upload

---
+++ level1_reverse_string/level1_README.md
@@ -0,0 +1,9 @@
+## Level 1: Reverse a String
+
+This program takes a string input from the user and reverses it.
+
+It uses Python string slicing to reverse the text easily.
+
+Example:
+Input: hello  
+Output: olleh
---
+++ level1_reverse_string/level1_reverse_string.py
@@ -0,0 +1,4 @@
+# Level 1: Reverse a string
+text = input("Enter a string: ")# Take input from user
+reversed_text = text[::-1]# Reverse the string using slicing
+print("Reversed string:", reversed_text)# Print the result
